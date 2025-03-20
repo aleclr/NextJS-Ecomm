c0740f8b17d4daf5c4850d57338eddfe74bdb368
alec <alecrondel@hotmail.com>
2025-03-20 17:58:51 -0300

    build: eslint config init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+// import pluginNext from "eslint-plugin-next"; // Import Next.js plugin
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  // pluginNext.configs.recommended, // Add Next.js recommended config
+  { 
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
