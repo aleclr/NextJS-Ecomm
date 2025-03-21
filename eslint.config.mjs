14a31c93dde50ffebeafd9839c2431bb31504409
alec <alecrondel@hotmail.com>
2025-03-21 16:56:26 -0300

    build: eslint 9 + prettier configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,36 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-// import pluginNext from "eslint-plugin-next"; // Import Next.js plugin
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import pluginReactHooks from "eslint-plugin-react-hooks"
+import pluginNext from "@next/eslint-plugin-next"
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    settings: { react: { version: "detect" } }
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  // pluginNext.configs.recommended, // Add Next.js recommended config
-  { 
+  eslintConfigPrettier,
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      "@next/next": pluginNext
+    },
+
     rules: {
       "react/react-in-jsx-scope": "off",
-    },
-  },
-];
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/prop-types": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules
+    }
+  }
+]
